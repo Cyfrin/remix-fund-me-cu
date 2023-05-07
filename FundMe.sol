fdcdc31a25599cb9b6d87aa36cd47916ea3df9c1
hardhat-freecodecamp <103076139+hardhat-freecodecamp@users.noreply.github.com>
2023-05-07 14:45:12 -0400

    Update FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -28,7 +28,7 @@ contract FundMe {
     }
     
     function getVersion() public view returns (uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         return priceFeed.version();
     }
     
