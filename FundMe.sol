de6ab8ea4939002bde5315de917841cce14d4f30
MostPow3rful <itskianshahi@gmail.com>
2024-06-26 12:37:36 +0330

    Changed the Imported Path
    Removed /shared/

--- FundMe.sol
+++ FundMe.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.18;
 
 // Note: The AggregatorV3Interface might be at a different location than what was in the video!
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
 error NotOwner();
