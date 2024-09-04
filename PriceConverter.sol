35d7b13ee0c495c32fbe1295d497294d1b9f9744
Vasiliy Gualoto <vasiliy@cromewar.com>
2024-09-04 09:46:45 -0500

    Update PriceConverter.sol
    Adding the correct import to the aggregatorv3interface

--- PriceConverter.sol
+++ PriceConverter.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol"; 
 
 // Why is this a library and not abstract?
 // Why not an interface?
